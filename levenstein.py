83da225fad33f5bc00871ce72bb0a7fc9d6f23d7
Sasha-nice <borisov.al.an@mail.ru>
2021-06-02 23:26:14 +0300

    cache added

--- levenstein.py
+++ levenstein.py
@@ -5,24 +5,23 @@
 
 def levenstein(a, b):
 
-
-	def f(a, b):
-    	return a != b
+    def f(a, b):
+        return a != b
 
     cache = {}
+
     def rec_leven(m, n):
-    	if (m, n) in cache:
-    		return cache[(m, n)]
+        if (m, n) in cache:
+            return cache[(m, n)]
         if m == 0:
-        	cache[(m, n)] = n
+            cache[(m, n)] = n
             return n
         if n == 0:
-        	cache[(m, n)] = m
+            cache[(m, n)] = m
             return m
         cur = min(rec_leven(m, n - 1) + 1, rec_leven(m - 1, n) + 1, rec_leven(m - 1, n - 1) + f(a[m - 1], b[n - 1]))
         cache[(m, n)] = cur
         return cur
-
 
     return rec_leven(len(a), len(b))
 
